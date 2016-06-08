e527a3c2bcf735ca7f333d7e2929386d36c6f245
bbrouwer <b.brouwer@student.avans.nl>
2016-06-08 15:38:32 +0200

    Setup

---
+++ fulladder/NodeFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fulladder
+{
+    class NodeFactory
+    {
+        private Dictionary<String, Type> _types;
+
+        public void AddNodeType(string name, Type type)
+        {
+            _types[name] = type;
+        }
+
+        //public INode CreateNode(string type, String identifier)
+        //{
+        //    Type t = _types[type];
+        //    INode n = new INode();
+        //}
+    }
+}
---
+++ fulladder/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fulladder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeParser tp = new TreeParser();
+            bool inputValid = false;
+
+            Console.WriteLine("Please enter tree location and press enter");
+            Console.WriteLine("For example : C:\\User\\Examplefolder\\Tree.txt");
+
+            while (!inputValid) {
+               
+                String input = Console.ReadLine();
+                inputValid = tp.readFile("C: \\Users\\bram\\School\\IDP\\CIRCUIT3.txt");
+                
+            }
+            tp.filterLines();
+            tp.printNodes();
+           
+            Console.ReadKey();
+
+        }
+    }
+}
