62a5e9697c0510a62f0f0e928ff03b4394e5ac2a
Kevin Hoogendam <khoogendam6@gmail.com>
2017-05-18 12:09:05 +0200

    nodes

---
+++ fulladder_bram_kevin/fulladder_bram_kevin/Model/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fulladder_bram_kevin.Model
+{
+    public abstract class Node
+    {
+        public List<int> inputs;
+        public int output;
+        public Node next;
+
+        public abstract void Run();
+    }
+}
